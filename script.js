935c9a9e0bf943d8cc62b4969c8d687d45074b85
Wish McCarty <joannamccarty10@gmail.com>
2019-03-01 11:47:42 -0500

    add minute and hour hands

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
 const secondHand = document.querySelector('.second-hand');
-console.log(secondHand);
+const minuteHand = document.querySelector('.min-hand');
+const hourHand = document.querySelector('.hour-hand');
+
 // get current seconds
 function setDate() {
   const now = new Date();
   const seconds = now.getSeconds();
-  const secondsDegrees = ((seconds / 60) * 360);
+  const minutes = now.getMinutes();
+  const hours = now.getHours();
+  const secondsDegrees = ((seconds / 60) * 360) + 90;
+  const minutesDegrees = ((minutes / 60) * 360) + 90;
+  const hourDegrees = ((hours / 12) * 360) + 90;
   secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+  minuteHand.style.transform = `rotate(${minutesDegrees}deg)`;
+  hourHand.style.transform = `rotate(${hourDegrees}deg)`;
   console.log(seconds);
 }
 // run setDate every second aka 1000 milliseconds
